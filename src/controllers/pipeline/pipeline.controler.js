1eba861276ac0df81897b843af661da7ca3db50e
AyanMrFranchies <ayankalaibabaateam@gmail.com>
2025-10-24 18:55:25 +0530

    updatePipelineId completed

--- src/controllers/pipeline/pipeline.controler.js
+++ src/controllers/pipeline/pipeline.controler.js
@@ -99,10 +99,9 @@ export const deletePipeline = async (req, res) => {
 export const updatePipelineId = async (req, res) => {
   try {
     const id = req.params?.id;
-    const { addStages, deleteStages, updateStagesName } = req.body;
+    const { addStages, deleteStages, updateStagesName, deleteTag } = req.body;
     const dbname = req.query?.dbname;
 
-    // console.log("updateStagesName", updateStagesName);
 
     if (!id) {
       return res.json(new ApiResponse(400, null, "Pipeline ID is required"));
@@ -145,12 +144,14 @@ export const updatePipelineId = async (req, res) => {
 
       return pipeline;
     }
+
     async function updateStagesNameFunction(pipeline) {
       let newdata = pipeline?.data || [];
-      console.log("firdt ",newdata)
+      
 
-      console.log("newdata",newdata[updateStagesName?.insertIndex])
-      const index = updateStagesName.insertIndex;
+      // console.log("newdata",newdata[updateStagesName?.insertIndex])
+      const index = updateStagesName?.insertIndex;
+      const tagIndex = updateStagesName.data?.updateTagName?.insertIndex
     //   if (newdata[index]) {
 
     //     if (updateStagesName.data?.Stage !== "") {
@@ -166,17 +167,63 @@ export const updatePipelineId = async (req, res) => {
       if (updateStagesName.data?.Stage && newdata[index].Stage !== undefined ) {
         newdata[index].Stage = updateStagesName.data.Stage;
       }
-    console.log("newdata ",newdata)
-      // pipeline.data = newdata;
-      // await pipeline.save();
+      if (
+        updateStagesName.data?.updateTagName &&
+        Array.isArray(newdata[index].tags) &&
+        newdata[index].tags[tagIndex] !== undefined
+      ) {
+        newdata[index].tags[tagIndex] = updateStagesName.data.updateTagName.tag;
+      }
+      
+
+      if (updateStagesName.data?.tags && Array.isArray(newdata[index].tags)) {
+        newdata[index].tags.push(...updateStagesName.data.tags);
+      }
+    // console.log("newdata ",newdata)
+      pipeline.data = newdata;
+      await pipeline.save();
 
       return pipeline;
     }
+
+    async function deleteTagFunction(pipeline) {
+      let newdata = pipeline.data || [];
+
+      const stageIndex = deleteTag?.Stage;
+      const tagIndex = deleteTag?.tag;
+
+      if (
+        newdata[stageIndex] &&
+        Array.isArray(newdata[stageIndex].tags) &&
+        newdata[stageIndex].tags[tagIndex] !== undefined
+      ) {
+
+        // console.log("newdata[stageIndex].tags :",newdata[stageIndex].tags)
+        
+        newdata[stageIndex].tags = newdata[stageIndex].tags.filter(
+          (_, i) => i !== tagIndex
+        );
+
+      }
+
+      pipeline.data = newdata;
+      await pipeline.save(); 
+
+      return pipeline;
+    }
+
 
     if (addStages && addStages.length > 0) {
       const updatedPipeline = await addStagesFunction(pipeline);
       return res.json(
         new ApiResponse(200, updatedPipeline, "Multiple stages inserted successfully")
+      );
+    }
+
+    if (deleteTag) {
+      const updatedPipeline = await deleteTagFunction(pipeline);
+      return res.json(
+        new ApiResponse(200, updatedPipeline, "Multiple stages deleted successfully")
       );
     }
 
@@ -186,6 +233,7 @@ export const updatePipelineId = async (req, res) => {
         new ApiResponse(200, updatedPipeline, "Multiple stages deleted successfully")
       );
     }
+    
     if (updateStagesName) {
       const updatedPipeline = await updateStagesNameFunction(pipeline);
       return res.json(
